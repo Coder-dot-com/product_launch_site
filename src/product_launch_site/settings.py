fcb6c14c65b1bf3689aea7bd4c14cf6d2add5323
Usman <usman17.s@hotmail.com>
2024-03-13 13:00:46 +0000

    remove repeat databases reference

--- src/product_launch_site/settings.py
+++ src/product_launch_site/settings.py
@@ -192,13 +192,6 @@ WSGI_APPLICATION = 'product_launch_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
