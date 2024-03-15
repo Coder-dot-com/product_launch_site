aa462155078c42ffdbc92db6895ce069177e5fd2
Usman <usman17.s@hotmail.com>
2024-03-15 17:39:31 +0000

    Progress on automated title image genration

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from decouple import config
 import requests
 import random
 import boto3
+import time
 headers = {
                 'Authorization': config('PEXELS_API_KEY')
             }
@@ -9,7 +10,7 @@ headers = {
             #Change query to try except with decreasing num of nouns
 query = "test"
 
-endpoint = f"https://api.pexels.com/v1/search?query={query}&per_page=5"
+endpoint = f"https://api.pexels.com/v1/search?query={query}&per_page=7"
 
 response = requests.get(url=endpoint, headers=headers)
 
@@ -26,10 +27,36 @@ r = requests.get(url, stream=True)
 session = boto3.Session()
 s3 = session.resource('s3')
 
-bucket_name = 'strategy-ahead-resized'
+bucket_name = 'strategy-ahead'
 key = f"temp-images/{url.split('/')[-1].split('?')[0]}" # key is the name of file on your bucket
 
 bucket = s3.Bucket(bucket_name)
 bucket.upload_fileobj(r.raw, key)
 
-#set up lambda to resize the image/clean
+#set up lambda to resize the image/clean (done)
+
+#add a wait to #access the cleaned image and process
+
+
+import shutil
+
+
+url = f'https://strategy-ahead.s3.amazonaws.com/resized-temp-images/{key.split('/')[-1]}'
+
+time.sleep(2)
+response = requests.get(url, stream=True)
+
+with open('img.png', 'wb') as out_file:
+    shutil.copyfileobj(response.raw, out_file)
+del response
+
+from PIL import Image
+
+im = Image.open("img.png")
+im.show()
+
+#crop iamge to required dimensions
+
+width, height = im.size
+
+center = width/2, height/2
