0dfa05ef2a016f2be52138af31c365d3bf537260
Usman <usman17.s@hotmail.com>
2024-03-18 16:30:00 +0000

    Add celery for FAQ generation

--- src/programmatic_pages/signals.py
+++ src/programmatic_pages/signals.py
@@ -7,71 +7,13 @@ from openai import OpenAI
 
 from decouple import config
 import time
+from .tasks import create_faq_product_templates
 
 @receiver(post_save, sender=ProductDevelopmentTemplate, dispatch_uid=f'save_product_template{uuid4()}')
 def save_profile(sender, instance, created, **kwargs):
     template = instance
-    from .tasks import my_task
 
-    result = my_task.delay(3, 5)
-
-    # if not FAQQuestionProductDevelopmentTemplate.objects.filter(product_development_template=template).exists():
-
-    #         client = OpenAI(
-    #         api_key=config("OPENAI_API_KEY"),
-    #         )
-    #         response = client.chat.completions.create(
-    #         model="gpt-4-turbo-preview",
-    #         messages=[
-    #             {
-    #             "role": "system",
-    #             "content": "Write in plaintext. Write like a human. Do not use markdown formatting. Do not use * or #. Do not talk about product development templates. Do not include numbers, use a new line for each question"
-    #             },
-    #             {
-    #             "role": "user",
-    #             "content": f"Write 5 frequently asked questions for a page titled: '{instance.keyword.keyword}'"
-    #             },
-    #         ],
-    #         temperature=1,
-    #         max_tokens=1000,
-    #         top_p=1,
-    #         frequency_penalty=0,
-    #         presence_penalty=0
-    #         )
-
-    #         content = (response.choices[0].message.content)
-
-    #         print('content', content)
-
-    #         questions = [x for x in content.split('\n') if x != ""]
-
-    #         for question in questions:
-    #             time.sleep(1)
-
-    #             client = OpenAI(
-    #             api_key=config("OPENAI_API_KEY"),
-    #             )
-    #             response = client.chat.completions.create(
-    #             model="gpt-4-turbo-preview",
-    #             messages=[
-    #                 {
-    #                 "role": "system",
-    #                 "content": "Write in plaintext. Write like a human. Do not use markdown formatting. Do not use * or #. Do not talk about product development templates."
-    #                 },
-    #                 {
-    #                 "role": "user",
-    #                 "content": f"Answer the following in approximately 250 words '{question}' for {instance.keyword.keyword}"
-    #                 },
-    #             ],
-    #             temperature=1.1,
-    #             max_tokens=512,
-    #             top_p=1,
-    #             frequency_penalty=0,
-    #             presence_penalty=0
-    #             )
-    #             answer = (response.choices[0].message.content)
-
-    #             faq_section = FAQQuestionProductDevelopmentTemplate.objects.create(question=question, answer=answer, product_development_template=instance)
+    result = create_faq_product_templates.delay(template.id)
 
 
 
--- src/programmatic_pages/tasks.py
+++ src/programmatic_pages/tasks.py
@@ -1,10 +1,72 @@
 from product_launch_site.celery import app
 from celery.utils.log import get_task_logger
+from .models import ProductDevelopmentTemplate, FAQQuestionProductDevelopmentTemplate
 
+from openai import OpenAI
+from decouple import config
+import time
 logger = get_task_logger(__name__)
 
 @app.task
-def my_task(arg1, arg2):
-    # Task logic here
-    result = arg1 + arg2
-    logger.info('hkjhkjhjkhtest', result)
+def create_faq_product_templates(product_template_object_id):
+    template = ProductDevelopmentTemplate.objects.get(id=product_template_object_id)
+
+    if not FAQQuestionProductDevelopmentTemplate.objects.filter(product_development_template=template).exists():
+
+            client = OpenAI(
+            api_key=config("OPENAI_API_KEY"),
+            )
+            response = client.chat.completions.create(
+            model="gpt-4-turbo-preview",
+            messages=[
+                {
+                "role": "system",
+                "content": "Write in plaintext. Write like a human. Do not use markdown formatting. Do not use * or #. Do not talk about product development templates. Do not include numbers, use a new line for each question"
+                },
+                {
+                "role": "user",
+                "content": f"Write 5 frequently asked questions for a page titled: '{template.keyword.keyword}'"
+                },
+            ],
+            temperature=1,
+            max_tokens=1000,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+            )
+
+            content = (response.choices[0].message.content)
+
+            print('content', content)
+
+            questions = [x for x in content.split('\n') if x != ""]
+
+            for question in questions:
+                time.sleep(1)
+
+                client = OpenAI(
+                api_key=config("OPENAI_API_KEY"),
+                )
+                response = client.chat.completions.create(
+                model="gpt-4-turbo-preview",
+                messages=[
+                    {
+                    "role": "system",
+                    "content": "Write in plaintext. Write like a human. Do not use markdown formatting. Do not use * or #. Do not talk about product development templates. Do not mention any other websites."
+                    },
+                    {
+                    "role": "user",
+                    "content": f"Answer the following in approximately 250 words '{question}' for {template.keyword.keyword}"
+                    },
+                ],
+                temperature=1.1,
+                max_tokens=512,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+                )
+                answer = (response.choices[0].message.content)
+
+                faq_section = FAQQuestionProductDevelopmentTemplate.objects.create(question=question, answer=answer, product_development_template=template)
+
+
